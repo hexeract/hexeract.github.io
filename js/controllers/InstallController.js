6736cc148da88554609ac2f046a8cc4fbd7589ae
Matias Pierobon <matias.pierobonm@gmail.com>
2016-01-10 15:39:03 -0300

    create and download blob

--- js/controllers/InstallController.js
+++ js/controllers/InstallController.js
@@ -1,17 +1,20 @@
-appInstaller.controller('InstallController', function($scope, $mdDialog, profileService, InstallerFactory) {
+appInstaller.controller('InstallController', function($scope, $window, $mdDialog, profileService, InstallerFactory) {
+
+  var target = angular.element('#downloader');
 
   $scope.distro = 'Ubuntu';
   $scope.distros = ['Ubuntu'];
+  $scope.url = "";
 
   $scope.download = function() {
     var prof = $scope.$parent.profiles[$scope.$parent.profile];
     var distro = $scope.distro;
-  };
 
-  $scope.install = function() {
-    var p = $scope.$parent.profiles[$scope.$parent.profile];
-    var distro = $scope.distro;
-
-    var repos, install, postInstall;
+    InstallerFactory.getInstaller(prof, distro).then(function (installer) {
+      var blob = new Blob([installer], {type: "application/x-sh;charset=utf-8"}),
+      $scope.url = $window.URL.createObjectURL(blob);
+      target.click();
+      $window.URL.revokeObjectURL($scope.url);
+    });
   };
 });
