6a5937272903a679b58393c291fa1a0a4ece73db
Matias Pierobon <matias.pierobonm@gmail.com>
2016-01-10 16:11:31 -0300

    Fix download

--- js/controllers/InstallController.js
+++ js/controllers/InstallController.js
@@ -1,16 +1,23 @@
-appInstaller.controller('InstallController', function($scope, $window, $mdDialog, profileService, InstallerFactory) {
+appInstaller.controller('InstallController', function($scope, $window, $document, $mdDialog, profileService, InstallerFactory) {
 
   $scope.distro = 'Ubuntu';
   $scope.distros = ['Ubuntu'];
   $scope.url = "";
 
   $scope.download = function() {
+    var a = $document.createElement("a");
+    $document.body.appendChild(a);
+    a.style = "display: none";
+
     var prof = $scope.$parent.profiles[$scope.$parent.profile];
     var distro = $scope.distro;
 
     InstallerFactory.getInstaller(prof, distro).then(function (installer) {
-      var blob = new Blob([installer], {type: "application/x-sh;charset=utf-8"});
-      $scope.url = $window.URL.createObjectURL(blob);
+      var blob = new Blob([installer], {type: "application/x-sh;charset=utf-8"}),
+          url = $window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = "script.sh";
+        a.click();
       $window.URL.revokeObjectURL($scope.url);
     });
   };
